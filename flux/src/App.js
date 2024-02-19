0c44cecf53508aa8c14602f8a3a584148e2dfe7e
Ryan Ebanks <ryankebanks@gmail.com>
2024-02-18 21:30:09 -0600

    Initial Commit

---
+++ flux/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App font-abc text-gray-900 bg-slate-100">
+      <header className="App-header">
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
