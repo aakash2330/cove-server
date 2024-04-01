4a0c70e1c330d962a6af879aa11bad6c71c52104
Ryan Ebanks <ryankebanks@gmail.com>
2024-03-31 21:27:53 -0500

    Added slide animations, going to fix the infinite text loop one

--- flux/src/components/Home.js
+++ flux/src/components/Home.js
@@ -1,23 +1,17 @@
-import heroLeft from '../images/banner-left.png';
-import heroRight from '../images/banner-right.png';
+import hero from '../images/Banner-Hero.png';
+// import heroRight from '../images/banner-right.png';
 import '../index.css';
 
 
 function Home() {
     return (
-        <div className='grid grid-cols-2 h-1/4'>
+        <div className='flex grow h-1/4'>
             <img
-                src={heroLeft}
+                src={hero}
                 alt='hero banner'
-                className='object-cover h-[675px] w-[900px]'
+                className='object-cover'
             />
-            <img
-                src={heroRight}
-                alt='hero banner zoomed in'
-                className='object-cover h-[675px] w-[900px]'
-            />
-        </div>
-
+            </div>
     );
 
 }
--- flux/src/components/ProductSale.js
+++ flux/src/components/ProductSale.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
@@ -15,10 +16,10 @@ const ProductSale = () => {
                 if (!response.ok) {
                     throw new Error(`Network response error: ${response.statusText}`);
                 }
-                    const data = await response.json();
-                    console.log('Data from API:', data); 
-                    setProducts(data);
-            // }
+                const data = await response.json();
+                console.log('Data from API:', data);
+                setProducts(data);
+                // }
             } catch (error) {
                 console.error('Error fetching data:', error);
                 setError('Could not fetch data');
@@ -45,29 +46,36 @@ const ProductSale = () => {
     }
 
     return (
-        <div className='flex flex-wrap justify-evenly m-4'>
+        <motion.div
+            className='flex flex-wrap justify-evenly my-4'
+            animate={{
+                x: ['-100%', '0%'],
+                opacity: [0, 1]
+            }}
+            transition={{ duration: 0.5, ease: 'linear' }}
+        >
             {products.map((product) => (
                 <li key={product.productId} className='list-none'>
                     <Link to={`/api/product/${product.productId}`}>
-                    <div className='flex-col m-4 w-[332px] h-[434px]'>
-                        <div className='w-[332px] h-[330px] bg-[#efefef]'>
-                        <img
-                            src={`/${product.img}`}
-                            alt='placeholder template'
-                            className='object-scale-down object-center w-[330px] h-[330px]'
-                        />
+                        <div className='flex-col m-4 w-[332px] h-[434px]'>
+                            <div className='w-[332px] h-[330px] bg-[#efefef]'>
+                                <img
+                                    src={`/${product.img}`}
+                                    alt='placeholder template'
+                                    className='object-scale-down object-center w-[330px] h-[330px]'
+                                />
+                            </div>
+                            <p className='text-base mt-4 font-light'>{product.title}</p>
+                            <div className='flex flex-row'>
+                                <p className='my-1 text-sm font-semibold'>${product.price}.00</p>
+                                <p className='justify-end inline-block text-sm font-semibold bg-white text-black px-6 my-1'>SALE</p>
+                            </div>
                         </div>
-                        <p className='text-base mt-4 font-light'>{product.title}</p>
-                        <div className='flex flex-row'>
-                        <p className='my-1 text-sm font-semibold'>${product.price}.00</p>
-                        <p className='justify-end inline-block text-sm font-semibold bg-white text-black px-6 my-1'>SALE</p>
-                        </div>
-                    </div>
-                </Link>
+                    </Link>
                 </li>
-    ))
-}
-        </div >
+            ))
+            }
+        </motion.div>
     );
 }
 
